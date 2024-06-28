095a65db972aedf72f3c10b40a4bf6598d4977d6
OutlawAlex <13350@utl.edu.mx>
2024-06-28 09:47:47 -0600

    alumnos

---
+++ src/app/alumno-table/alumno-table.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from "@angular/core";
+import { AlumnoService } from "../alumno.service";
+
+@Component({
+  selector: "app-alumno-table",
+  templateUrl: "./alumno-table.component.html",
+  styleUrl: "./alumno-table.component.css",
+})
+export class AlumnoTableComponent implements OnInit {
+  alumnos = [];
+  displayedColumns: string[] = [
+    "Id",
+    "Nombre",
+    "ApellidoPaterno",
+    "ApellidoMaterno",
+    "Direccion",
+    "Matricula",
+  ];
+
+  constructor(private alumnosService: AlumnoService) {}
+
+  ngOnInit() {
+    this.getRegistros();
+
+    // Escuchar las actualizaciones
+    this.alumnosService.getAlumnos().subscribe(() => {
+      this.getRegistros();
+    });
+  }
+
+  getRegistros() {
+    this.alumnosService.getAlumnos().subscribe((data) => {
+      this.alumnos = data;
+    });
+  }
+}
